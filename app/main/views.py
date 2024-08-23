cac0d6be592086f7e75c67b043b61f964367c100
Ana Caroline <140763719+aanaca@users.noreply.github.com>
2024-08-22 21:00:12 -0300

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -6,26 +6,25 @@ from . import main
 from .forms import NameForm
 
 
-@main.route('/', methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+    user_all = User.query.all();
+    role_all = Role.query.all();
+    print(user_all);
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
+        user = User.query.filter_by(username=form.name.data).first()                        
         if user is None:
-            user = User(username=form.name.data)
+            user_role = Role.query.filter_by(name=form.role.data).first();
+            user = User(username=form.name.data, role=user_role);
             db.session.add(user)
             db.session.commit()
             session['known'] = False
-            print('FLASKY_ADMIN: ' + str(current_app.config['FLASKY_ADMIN']), flush=True)
-            if current_app.config['FLASKY_ADMIN']:
-                #send_email(current_app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
-                print('Enviando mensagem...', flush=True)
-                send_simple_message([current_app.config['FLASKY_ADMIN'], "flaskaulasweb@zohomail.com"], 'Novo usuário', form.name.data)
-                print('Mensagem enviada...', flush=True)
         else:
             session['known'] = True
         session['name'] = form.name.data
-        return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False));
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False),
+                           user_all=user_all, role_all = role_all);
