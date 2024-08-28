92da24550af4132208b191dc82a291fb36a59eb6
Ana Caroline <140763719+aanaca@users.noreply.github.com>
2024-08-28 14:52:34 -0300

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
 from flask import render_template, session, redirect, url_for, current_app
 from .. import db
-from ..models import User
-from ..email import send_email, send_simple_message
+from ..models import User, Role
+from ..email import sendgrid_send_message
 from . import main
 from .forms import NameForm
+
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -11,7 +12,15 @@ def index():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
-            user = User(username=form.name.data)
+
+            user_role = Role.query.filter_by(name='User').first()
+
+            if user_role is None:
+                user_role = Role(name='User')
+                db.session.add(user_role)
+                db.session.commit()
+
+            user = User(username=form.name.data, role=user_role)
             db.session.add(user)
             db.session.commit()
             session['known'] = False
@@ -19,12 +28,28 @@ def index():
             if current_app.config['FLASKY_ADMIN']:
                 #send_email(current_app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
                 print('Enviando mensagem...', flush=True)
-                send_simple_message([current_app.config['FLASKY_ADMIN'], "flaskaulasweb@zohomail.com"], 'Novo usuário', form.name.data)
+                sendgrid_send_message([str(current_app.config['FLASKY_ADMIN']), "flaskaulasweb@zohomail.com"], '[Flask App] Novo Usuário Cadastrado', form.name.data)
                 print('Mensagem enviada...', flush=True)
         else:
+
+            if user.role is None:
+                user_role = Role.query.filter_by(name='User').first()
+
+                if user_role is None:
+                    user_role = Role(name='User')
+                    db.session.add(user_role)
+                    db.session.commit()
+
+                user.role = user_role
+                db.session.commit()
+
             session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('.index'))
+
+    users = User.query.all()
+    roles = Role.query.all()
+
     return render_template('index.html',
                            form=form, name=session.get('name'),
-                           known=session.get('known', False));
+                           known=session.get('known', False), users=users, roles=roles)
